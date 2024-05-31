4d987daf1a58bfeb32bcac0285dbbd7e566c5b1e
JUAN JESUS TREJO RUIZ <AL03049753@tecmilenio.mx>
2024-05-31 15:03:07 -0700

    Vambio en el lector del CSV

--- Data/Model/CSVReader.cs
+++ Data/Model/CSVReader.cs
@@ -7,13 +7,13 @@ using Microsoft.EntityFrameworkCore;
 using OyeIap.Server.Pages;
 using System.Text;
 
-
+//Aqui estaba intentando crear el lector del CSV, e hice mas o menos el de padrinos
 
 namespace OyeIap.Server.Data.Model
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CSVReader : ControllerBase
+    public class CSVReader : Controller
     {
         private readonly IWebHostEnvironment _webHostEnvironment;
 
@@ -36,17 +36,8 @@ namespace OyeIap.Server.Data.Model
                 // Obtiene el archivo subido
                 var file = Request.Form.Files[0];
 
-                // Verifica si el archivo es un archivo CSV
-                if (file.ContentType != "text/csv")
-                {
-                    return BadRequest("Uploaded file must be a CSV file.");
-                }
-
                 // Obtiene la ruta de la carpeta de carga en el servidor
                 string uploadFolderPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads");
-
-                // Crea la carpeta de carga si no existe
-                Directory.CreateDirectory(uploadFolderPath);
 
                 // Genera un nombre de archivo único para evitar conflictos
                 string uploadedFileName = Guid.NewGuid().ToString() + ".csv";
@@ -54,13 +45,45 @@ namespace OyeIap.Server.Data.Model
                 // Combina la ruta de la carpeta de carga con el nombre de archivo generado
                 string csvFilePath = Path.Combine(uploadFolderPath, uploadedFileName);
 
-                // Guarda el archivo subido en el servidor
-                using (var stream = new FileStream(csvFilePath, FileMode.Create))
-                {
-                    file.CopyTo(stream);
-                }
+                string linea;
 
                 // Procesa el archivo CSV (aquí puedes implementar la lógica para leer y procesar el archivo)
+                System.IO.StreamReader archivo = new System.IO.StreamReader(csvFilePath);
+                archivo.ReadLine();
+
+                //No terminado
+                /*while((linea = archivo.ReadLine()) != null)
+                {
+                    string[] fila = linea.Split(',');
+                    var tutor = new Tutor
+                    {
+                        Nombre = fila[0],
+                        NombreFiscal = RandomOne(_nombres),
+                        ApellidoPaterno = apellidoPaterno,
+                        ApellidoMaterno = RandomOne(_apellidos),
+                        NombreP1 = RandomOne(_nombres),
+                        ApellidoPaternoP1 = apellidoPaterno,
+                        ApellidoMaternoP1 = RandomOne(_apellidos),
+                        Correo = $"{RandomOne(_nombres).Replace(" ", "").ToLower()}@example.com",
+                        Direccion = sameAddress ? alumno.Direccion : RandomOne(_direcciones),
+                        Telefono = $"123456789{i}",
+                        TelefonoEmpresa = $"123456789{i}",
+                        RFC = $"123456789{i}",
+                        PatrocinioActivo = random.Next(2) == 0,
+                        PersonaF = random.Next(2) == 0,
+                        PersonaM = random.Next(2) == 0,
+                        PersonaFisica = "Pago",
+                        PersonaMoral = "Pago",
+                        EmpresaNombre = RandomOne(_nombresEmpresa),
+                        TipoEmp = RandomEnumValue<TipoEmpresa>(),
+                        DetallesAyuda = "Detalles de ayuda",
+                        Donacion = random.Next(100, 1000),
+                        InformacionExtra = "Informacion Extra",
+                        comentarios = "comentarios",
+                        RegimenFiscal = "Demás ingresos",
+                        CorreoEmpresa = $"{RandomOne(_nombres).Replace(" ", "").ToLower()}@example.com"
+                    };
+                }*/
 
                 return Ok("CSV file uploaded successfully.");
             }
